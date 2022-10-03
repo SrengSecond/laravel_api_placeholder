b56abe61bcb20c4c71d583f2ae806ea95f12d9f0
SrengSecond <stickercenter2@gmail.com>
2022-10-03 08:49:27 +0700

    Add CRUD for post model

---
+++ app/Http/Controllers/Api/JobController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreJobRequset;
+use App\Http\Resources\JobResource;
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        $jobs = Job::all();
+        /*dd($jobs->toArray());*/
+        return JobResource::collection($jobs);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return JobResource
+     */
+    public function store(StoreJobRequset $request)
+    {
+        $jobs = Job::create($request->all());
+        return new JobResource($jobs);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Job  $job
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Job $job)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Job  $job
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Job $job)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Job  $job
+     * @return JobResource
+     */
+    public function update(Request $request, Job $job)
+    {
+        $job->update($request->all());
+
+        return new JobResource($job);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Job  $job
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Job $job)
+    {
+        $job->delete();
+        return response(null, 204);
+    }
+}
